83027ea898b66ada697253a5ea680d26da194800
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2024-09-05 16:27:47 -0500

    build

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -5,7 +5,6 @@ echo "Install SFDX Scanner"
 #echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner@latest
 
 npm cache clean --force
-npm install -g yarn
 
 ##echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
