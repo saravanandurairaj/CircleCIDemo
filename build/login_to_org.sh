0c38ce7606d5d99836566c5f64b4494b26b7de06
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2024-09-06 12:54:37 -0500

    login

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -1,6 +1,9 @@
 echo "Logging into Salesforce Org"
-mkdir keys
-echo $CERT_KEY | base64 -di > keys/server.key
+# mkdir keys
+# echo $CERT_KEY | base64 -di > keys/server.key
+
+# echo "Authenticating org"
+# sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -1,3 +1,18 @@
+# Authenticate to salesforce
+#echo "Authenticating..."
+#sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+
+#Create a scratch org
+#echo "Creating the Scratch Org..."
+#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+
+#sfdx force:source:push -u ${CIRCLE_BRANCH}
+
+#echo "Coverting source to metadata format"
+#sfdx force:source:convert -d test_code -r force-app
+
+#echo "Deploying code to org"
+#sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
 
 echo "Authenticating org"
 sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
