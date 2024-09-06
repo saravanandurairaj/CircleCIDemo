0ae606b7956168c1acd554a87242402e6341b48f
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2024-09-06 16:21:21 -0500

    demo

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -20,5 +20,8 @@ sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.k
 echo "Deploying source to org"
 sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
 
-echo "Testing code in org"
+#echo "Run All Test Class in org"
 #sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
+
+echo "Run Specific Test Class in org"
+sfdx force:apex:test:run -n DemoLWCControllerTest,SampleControllerTest  --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
