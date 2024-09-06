e43729b856a1142bd14f5bb5ed897a377d28d51b
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2024-09-06 12:39:36 -0500

    scanner

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -2,9 +2,9 @@ echo "Installing JDK"
 #sudo apt-get install openjdk-8-jdk
 
 echo "Install SFDX Scanner"
-echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner@latest
+#echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner@latest
 
 #npm cache clean --force
 
 echo "Running SFDX Scanner"
-npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
+#npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
