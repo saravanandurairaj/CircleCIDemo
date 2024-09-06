485b77d761d3b2c7b7702a5e8c7a0f39fe2c255f
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2024-09-06 13:00:07 -0500

    3

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -1,9 +1,6 @@
 echo "Logging into Salesforce Org"
-# mkdir keys
-# echo $CERT_KEY | base64 -di > keys/server.key
-
-# echo "Authenticating org"
-# sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+mkdir keys
+echo $CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
