7226f24fa616f449e778827c30c9aa5069805ebd
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2024-09-13 16:41:56 -0500

    0

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -6,6 +6,6 @@ echo "Deploying code to org"
 # sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w -1 -l RunLocalTests
 
 echo "Run Specific Test Class to org"
-sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w 1000  --verbose --loglevel DEBUG -r DemoLWCControllerTest,SampleControllerTest --testlevel RunSpecifiedTests > /tmp/artifacts/deployResult.txt
+sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w 1000  --verbose -r DemoLWCControllerTest,SampleControllerTest --testlevel RunSpecifiedTests > /tmp/artifacts/deployResult.txt
 
 #sfdx force:apex:test:run -n DemoLWCControllerTest,SampleControllerTest  --testlevel RunSpecifiedTests --outputdir /tmp/artifacts --resultformat tap --targetusername DevHub
